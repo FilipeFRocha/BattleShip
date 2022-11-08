0591754079eb806deb61275bf1a4a48371ca1481
Crossbull <117479453+Crossbull@users.noreply.github.com>
2022-11-08 17:48:00 +0000

    asdasd

--- BattleShip/Game/battleship.js
+++ BattleShip/Game/battleship.js
@@ -477,25 +477,14 @@ var bot = {
 
 //  Create the games grids and layout
 $(document).ready(function() {
-	for (var i = 1; i <= 100; i++) {
+
+	cellLinha = 10;
+	grid = cellLinha * cellLinha;
+
+	for (var i = 1; i <= grid; i++) {
 		// The number and letter designators
-		if (i < 11) {
-			$(".top").prepend("<span class='aTops'>" + Math.abs(i - 11) + "</span>");
-			$(".bottom").prepend("<span class='aTops'>" + Math.abs(i - 11) + "</span>");
-			$(".grid").append("<li class='points offset1 " + i + "'><span class='hole'></span></li>");
-		} else {
-			$(".grid").append("<li class='points offset2 " + i + "'><span class='hole'></span></li>");
-		}
-		if (i == 11) {
-			$(".top").prepend("<span class='aTops hidezero'>" + Math.abs(i - 11) + "</span>");
-			$(".bottom").prepend("<span class='aTops hidezero'>" + Math.abs(i - 11) + "</span>");
-		}
-		if (i > 90) {
-			$(".top").append("<span class='aLeft'>" + 
-								String.fromCharCode(97 + (i - 91)).toUpperCase() + "</span>");
-			$(".bottom").append("<span class='aLeft'>" + 
-								String.fromCharCode(97 + (i - 91)).toUpperCase() + "</span>");
-		}
+		$(".grid").append("<li class='points offset1 " + i + "'><span class='hole'></span></li>");
+		
 	}
 	$(".text").text(output.welcome);
 })
